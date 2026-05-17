44866f1c76ac28cf531bdb841eee30aaf90f0788
Sann2267 <ibnuadzim2@gmail.com>
2026-05-17 15:55:29 +0700

    Update secrets.h

--- firmware/secrets.h
+++ firmware/secrets.h
@@ -1,26 +1,4 @@
-#include <pgmspace.h>
-
-#define SECRET
-#define THINGNAME "ESP32_DHT22"
-
-const char WIFI_SSID[] = "NAMA_WIFI";
-const char WIFI_PASSWORD[] = "PASSWORD_WIFI";
-const char AWS_IOT_ENDPOINT[] = "xxxxx-ats.iot.region.amazonaws.com";
-
-static const char AWS_CERT_CA[] PROGMEM = R"EOF(
------BEGIN CERTIFICATE-----
-Tuliskan isi AmazonRootCA1.pem di sini
------END CERTIFICATE-----
-)EOF";
-
-static const char AWS_CERT_CRT[] PROGMEM = R"EOF(
------BEGIN CERTIFICATE-----
-Tuliskan isi xxxxx-certificate.pem.crt di sini
------END CERTIFICATE-----
-)EOF";
-
-static const char AWS_CERT_PRIVATE[] PROGMEM = R"EOF(
------BEGIN RSA PRIVATE KEY-----
-Tuliskan isi xxxxx-private.pem.key di sini
------END RSA PRIVATE KEY-----
-)EOF";
+const char* ssid = "II";
+const char* password = "Qwerty123";
+const char* mqtt_server = "13.238.95.89";  
+const int mqtt_port = 1883;
